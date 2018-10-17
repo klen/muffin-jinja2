63336a0dd97f78f879b8563bb454dc39dd1f1264
Kirill Klenov <horneds@gmail.com>
2018-10-17 16:09:11 +0300

    Bump version: 0.1.1 → 0.2.0

--- muffin_jinja2.py
+++ muffin_jinja2.py
@@ -8,7 +8,7 @@ from muffin.utils import to_coroutine
 from ujson import dumps
 
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __project__ = "muffin-jinja2"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
