05fd355af5a5202aca1ceb8569a51ba48fd3cca7
Kirill Klenov <horneds@gmail.com>
2021-02-05 17:14:50 +0300

    Bump version: 0.7.1 → 0.7.2

--- muffin_jinja2/__init__.py
+++ muffin_jinja2/__init__.py
@@ -8,7 +8,7 @@ from muffin.plugin import BasePlugin, PluginException
 from muffin.utils import to_awaitable
 
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 __project__ = "muffin-jinja2"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
