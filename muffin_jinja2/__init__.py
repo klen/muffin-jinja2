200ba7ce1d80f5956555fb84788990ad6c4a12b5
Kirill Klenov <horneds@gmail.com>
2023-02-20 13:09:20 +0700

    feat: wip

--- muffin_jinja2/__init__.py
+++ muffin_jinja2/__init__.py
@@ -1,11 +1,12 @@
 """Muffin-Jinja2 -- Jinja2 template engine for Muffin framework."""
-import typing as t
-import jinja2
 from inspect import isawaitable
+from typing import Any, Callable, Dict, List, Literal, Optional, TypeVar, Union, overload
+from warnings import warn
 
 from muffin import Application
 from muffin.plugins import BasePlugin, PluginException
 
+import jinja2
 
 __version__ = "1.4.0"
 __project__ = "muffin-jinja2"
@@ -13,30 +14,31 @@ __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
 
 
-F = t.TypeVar('F', bound=t.Callable[..., t.Any])
+TVFn = TypeVar("TVFn", bound=Callable)
+TEnvParams = Literal["globals", "filters", "policies", "tests"]
 
 
 class Plugin(BasePlugin):
 
     """The class is used to control the jinja2 integration to Muffin application."""
 
-    name: str = 'jinja2'
+    name: str = "jinja2"
     defaults = {
-        'auto_reload': False,
-        'cache_size': 50,
-        'extensions': (),
-        'loader': None,
-        'encoding': 'utf-8',
-        'template_folders': ['templates'],
+        "auto_reload": False,
+        "cache_size": 50,
+        "extensions": (),
+        "loader": None,
+        "encoding": "utf-8",
+        "template_folders": ["templates"],
     }
 
     Error = jinja2.TemplateError
 
-    def __init__(self, app: Application = None, **options):
+    def __init__(self, app: Optional[Application] = None, **options):
         """Initialize the plugin."""
-        self.env: t.Optional[jinja2.Environment] = None
-        self.providers: t.List[t.Callable] = []
-        self.receivers: t.List[t.Callable[[t.Union[str, jinja2.Template], t.Dict], t.Any]] = []
+        self.env: Optional[jinja2.Environment] = None
+        self.ctx_providers: List[Callable] = []
+        self.receivers: List[Callable[[Union[str, jinja2.Template], Dict], Any]] = []
 
         super().__init__(app, **options)
 
@@ -45,8 +47,11 @@ class Plugin(BasePlugin):
         super().setup(app, **options)
 
         if not self.cfg.loader:
-            self.cfg.update(loader=jinja2.FileSystemLoader(
-                self.cfg.template_folders, encoding=self.cfg.encoding))
+            self.cfg.update(
+                loader=jinja2.FileSystemLoader(
+                    self.cfg.template_folders, encoding=self.cfg.encoding
+                )
+            )
 
         self.env = jinja2.Environment(
             auto_reload=self.cfg.auto_reload,
@@ -54,69 +59,85 @@ class Plugin(BasePlugin):
             extensions=self.cfg.extensions,
             loader=self.cfg.loader,
         )
-        self.env.globals['app'] = app
+        self.env.globals["app"] = app
 
-        @self.register
+        @self.add_global
         @jinja2.pass_context
         def debug(ctx, value=None):
             """Debug current context to template."""
             return jinja2.filters.do_pprint(value is None and ctx or value)
 
-    def context_processor(self, func: F) -> F:
+    @overload
+    def __register__(self, obj: TVFn, type: TEnvParams) -> TVFn:
+        ...
+
+    @overload
+    def __register__(self, obj: str, type: TEnvParams) -> Callable[[TVFn], TVFn]:
+        ...
+
+    def __register__(self, obj, type: TEnvParams = "globals"):
+        if self.env is None:
+            raise PluginException("The plugin must be installed to application.")
+
+        def wrapper(func):
+            env_name = func.__name__
+            if isinstance(obj, str):
+                env_name = obj
+            ctx = getattr(self.env, type)
+            ctx[env_name] = func
+            return func
+
+        if callable(obj):
+            return wrapper(obj)
+
+        return wrapper
+
+    def add_global(self, obj):
+        """Register function to globals.
+
+        ::
+            @jinja2.add_global
+            def my_global():
+                return {...}
+
+        """
+        return self.__register__(obj, "globals")
+
+    def add_filter(self, obj):
+        """Register function to filters.
+
+        ::
+            @jinja2.add_filter
+            def my_filter():
+                return {...}
+
+        """
+        return self.__register__(obj, "filters")
+
+    def add_context(self, func: TVFn) -> TVFn:
         """Decorate a given function to use as a context processor.
 
         ::
-            @jinja2.context_processor
+            @jinja2.add_context
             def my_context():
                 return {...}
         """
-        self.providers.append(func)
+        self.ctx_providers.append(func)
         return func
 
-    def register(self, name: t.Union[F, str]) -> t.Callable[[F], F]:
-        """Register function to globals."""
+    async def render(self, path: Union[str, jinja2.Template], **context) -> str:
+        """Render a template with the given context.
+
+        :param path: Path to the template or a template object.
+        :param context: Context to render the template.
+
+        """
         if self.env is None:
-            raise PluginException('The plugin must be installed to application.')
-
-        def wrapper(func):
-            env_name = func.__name__
-            if isinstance(name, str):
-                env_name = name
-            if callable(func):
-                self.env.globals[env_name] = func
-            return func
-
-        if callable(name):
-            return wrapper(name)
-
-        return wrapper
-
-    def filter(self, name: t.Union[F, str]) -> t.Callable[[F], F]:
-        """Register function to filters."""
-        if self.env is None:
-            raise PluginException('The plugin must be installed to application.')
-
-        def wrapper(func):
-            env_name = func.__name__
-            if isinstance(name, str):
-                env_name = name
-            if callable(func):
-                self.env.filters[env_name] = func
-            return func
-
-        if callable(name):
-            return wrapper(name)
-
-        return wrapper
-
-    async def render(self, path: t.Union[str, jinja2.Template], **context) -> str:
-        """Render a template with context."""
-        if self.env is None:
-            raise PluginException('Initialize the plugin first.')
+            raise PluginException("Initialize the plugin first.")
 
         template = self.env.get_template(path)
         ctx = dict(self.env.globals)
-        for provider in self.providers:
+        for provider in self.ctx_providers:
             ctx_ = provider()
             if isawaitable(ctx_):
                 ctx_ = await ctx_
@@ -127,3 +148,20 @@ class Plugin(BasePlugin):
             reciever(path, ctx)
 
         return template.render(**ctx)
+
+    # Depricated methods and properties
+
+    def register(self, obj):
+        """Register function to globals."""
+        warn("Use `add_register` instead of `register`", DeprecationWarning, 2)
+        return self.__register__(obj, "globals")
+
+    def filter(self, obj):
+        """Register function to filters."""
+        warn("Use `add_filter` instead of `filter`", DeprecationWarning, 2)
+        return self.__register__(obj, "filters")
+
+    def context_processor(self, func):
+        """Decorate a given function to use as a context processor."""
+        warn("Use `add_context` instead of `context_processor`", DeprecationWarning, 2)
+        return self.add_context(func)
--- tests/test_muffin_jinja2.py
+++ tests/test_muffin_jinja2.py
@@ -1,39 +1,40 @@
-import jinja2
 import muffin
 import pytest
 
+import jinja2
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def app():
     from muffin_jinja2 import Plugin as Jinja2
 
-    app = muffin.Application(name='jinja2', jinja2_template_folders=['tests'])
+    app = muffin.Application(name="jinja2", jinja2_template_folders=["tests"])
     jinja2 = Jinja2(app)
-    assert jinja2.cfg.template_folders == ['tests']
+    assert jinja2.cfg.template_folders == ["tests"]
 
-    @jinja2.context_processor
+    @jinja2.add_context
     def global_context():
-        return {'global': 'done'}
+        return {"global": "done"}
 
-    @jinja2.register
-    def sum(a, b):
+    @jinja2.add_global
+    def sum(a: int, b: int) -> int:
         return a + b
 
-    @jinja2.register('div')
+    @jinja2.add_global("div")
     def _div_(a, b):
         return a // b
 
-    @jinja2.filter
+    @jinja2.add_filter
     def test(test, a, b=None):
         return a if test else b
 
-    @app.route('/')
+    @app.route("/")
     async def index(request):
-        return await jinja2.render('template.html', **request.url.query)
+        return await jinja2.render("template.html", **request.url.query)
 
-    @app.route('/unknown')
+    @app.route("/unknown")
     async def unknown(request):
-        return await jinja2.render('unknown.html', **request.url.query)
+        return await jinja2.render("unknown.html", **request.url.query)
 
     return app
 
@@ -41,23 +42,23 @@ def app():
 async def test_muffin_jinja2(app, client):
     async with client.lifespan():
 
-        res = await client.get('/', query={'name': 'jinja2'})
+        res = await client.get("/", query={"name": "jinja2"})
         assert res.status_code == 200
 
         text = await res.text()
-        assert '<h1>Hello jinja2!</h1>' in text
-        assert '<p>8</p>' in text
-        assert '<p>3</p>' in text
-        assert '<b>done</b>' in text
-        assert '<i>yes</i>' in text
-        assert '<muffin.Application: jinja2>' in text
+        assert "<h1>Hello jinja2!</h1>" in text
+        assert "<p>8</p>" in text
+        assert "<p>3</p>" in text
+        assert "<b>done</b>" in text
+        assert "<i>yes</i>" in text
+        assert "<muffin.Application: jinja2>" in text
 
-        res = await client.get('/unknown')
+        res = await client.get("/unknown")
         assert res.status_code == 500
 
-        plugin = app.plugins['jinja2']
+        plugin = app.plugins["jinja2"]
 
-        assert await plugin.render(jinja2.Template('OK'))
+        assert await plugin.render(jinja2.Template("OK"))
 
 
 # pylama:ignore=D
